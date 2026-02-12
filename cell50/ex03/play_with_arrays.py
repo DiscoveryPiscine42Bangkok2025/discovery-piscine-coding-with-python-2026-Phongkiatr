f5357791bdbd63b420be62a2b02da25ec2291996
Phongkiatr <phongkiat2548@gmail.com>
2026-02-12 21:11:31 +0700

    submit

---
+++ cell50/ex03/play_with_arrays.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_array = set()
+
+for value in original_array:
+    if value > 5:
+        new_array.add(value + 2)
+
+print(original_array)
+print(new_array)
